0e8016791ee4aa47c92da1dd8a0dc750545ab9d6
Ricard Zarco <ricard.zarco@bsc.es>
2021-01-19 19:43:07 +0100

    dTLB added, needs some tunning still

--- src/rtl/segre_dtlb.sv
+++ src/rtl/segre_dtlb.sv
@@ -33,7 +33,6 @@ module segre_dtlb (
     //OUT
     output logic tlb_miss_o,
     output logic [PHYSICAL_ADDR_SIZE - 1:0] paddr_o
-    //TO FILL maybe
 );
 
 
@@ -53,46 +52,58 @@ tlb_entry [TLB_ENTRIES] tlb_entries;
 
 //Auxiliar vars
 //TLB index/tag/offsset generated from vaddr_i
-logic [TLB_ENTRY_BITS - 1:0] vaddr_i_index; 
+logic [TLB_ENTRY_BITS - 1:0] vaddr_i_index;
 logic [TLB_TAG_BITS - 1:0] vaddr_i_tag;
 logic [PAGE_OFFSET_BITS - 1:0] vaddr_i_offset;
 
-
+//Intermediate signals
+logic tlb_miss;
 
 //-----------------------------------------------------//
 //--------------------BEHAVIOR-------------------------//
 //-----------------------------------------------------//
 
-//Initialize full TLB as invalid
-always_ff @(posedge clk_i, negedge rsn_i) begin
-    if(!rsn_i) begin
-        //This for might be overkill, but I wasn't sure that assigning 0 YOLO-style would work with a struct array
-        for(int i =  0; i <= TLB_ENTRIES-1 ; i++) begin
-            tlb_entries[i].valid <= 1'b0;
-        end
-    end
-end
-
-//Generate TLB index/tag from vaddr_i
 always_comb begin
+    //Detect HIT/MISS
+    tlb_miss = tlb_entries[vaddr_i_index].valid && vaddr_i_tag == tlb_entries[vaddr_i_index].vpage[VIRT_PAGE_BITS - 1: TLB_ENTRY_BITS] ? 0 : 1;    
+    
+    //Generate TLB index/tag from vaddr_i
     vaddr_i_index = vaddr_i[WORD_SIZE - TLB_TAG_BITS - 1:PAGE_OFFSET_BITS];
     vaddr_i_tag = vaddr_i[WORD_SIZE - 1:TLB_TAG_BITS + PAGE_OFFSET_BITS];
     vaddr_i_offset = vaddr_i[PAGE_OFFSET_BITS - 1:0];
 end
 
-//Detect HIT/MISS
-//If HIT -> puke out physical address
-//If MISS -> puke out tlb_miss_o = 1, physical_address may be garbage 
-assign tlb_miss_o = tlb_entries[vaddr_i_index].valid && vaddr_i_tag == tlb_entries[vaddr_i_index].vpage[VIRT_PAGE_BITS - 1: TLB_ENTRY_BITS] ? 0 : 1;
-assign paddr_o = {tlb_entries[vaddr_i_index].ppage, vaddr_i_offset};
-
-//Write new translation into TLB if we see a write_enable (this is absolutely theoretical and probably wrong)
 always_ff @(posedge clk_i, negedge rsn_i) begin
-    if (write_enable_i) begin
-        tlb_entries[vaddr_i_index].ppage <= ppage_i;
-        tlb_entries[vaddr_i_index].vpage <= {vaddr_i_tag, vaddr_i_index};
-        tlb_entries[vaddr_i_index].valid <= 1; 
+    //Initialize full TLB
+    if(!rsn_i) begin
+        for(int i =  0; i < TLB_ENTRIES ; i++) begin
+            tlb_entries[i].valid <= 1'b0;
+        end
+    end
+
+    else begin
+        //Miss handling, it should produce an exception and then call the OS 
+        //but we will just do the conversion+allocation here for the time being
+        if (tlb_miss) begin
+            tlb_entries[vaddr_i_index].valid = 1'b1;
+            tlb_entries[vaddr_i_index].vpage = vaddr_i[WORD_SIZE - 1 : VIRT_PAGE_BITS];
+            tlb_entries[vaddr_i_index].ppage = tlb_entries[vaddr_i_index].vpage[PHYS_PAGE_BITS - 1 : 0] + 8'h08;
+        end
     end
 end
+
+//Write new translation into TLB if we see a write_enable (this is absolutely theoretical and probably wrong)
+//NOT USED UNTIL EXCEPTIONS WORKING
+//always_ff @(posedge clk_i, negedge rsn_i) begin
+//    if (write_enable_i) begin
+//        tlb_entries[vaddr_i_index].ppage <= ppage_i;
+//        tlb_entries[vaddr_i_index].vpage <= {vaddr_i_tag, vaddr_i_index};
+//        tlb_entries[vaddr_i_index].valid <= 1; 
+//    end
+//end
+
+assign tlb_miss_o = tlb_miss;
+assign paddr_o = {tlb_entries[vaddr_i_index].ppage, vaddr_i_offset};
+
 
 endmodule
